57b1e381d7388515374303fc30d6f148405b64e6
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-05-17 19:19:01 +0200

    [TASK] add new google fonts in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundImage: { 
+        'header-texture': "url('/public/images/header_img.jpg')",
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)']
       },
     },
   },
