7e5bb978a2fe8552800fccff25825c32117d2a95
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-05-24 18:52:00 +0200

    [TASK] create main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Games Store Nextjs</h1>
-    </main>
+    <>
+      <h1 className="text-6xl font-bold text-center text-sky-700 ">Games Store Nextjs</h1>
+    </>
   )
 }
