0acc68268c13b30f70662855fdc165e262590d82
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-05-25 18:59:50 +0200

    [TASK] create some new types

---
+++ src/types/Games.tsx
@@ -0,0 +1,26 @@
+type Cover = {
+  data: {
+    attributes: {
+      name: string;
+      alternativeText: string | null,
+      caption: string | null,
+      url: string;
+    };
+  };
+};
+
+type Games = {
+  id: number;
+  attributes: {
+    title: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    description: string;
+    url: string;
+    price: number;
+    cover_image: Cover;
+  };
+};
+
+export default Games;
