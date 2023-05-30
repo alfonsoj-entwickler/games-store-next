41cc309a1c4c36ef50140bd453e229bff7a2fe0a
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-05-30 19:29:40 +0200

    [TASK] add fetch function to load data in blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,29 @@
 
+export const metadata = {
+  title: "Blog Game",
+  description: "Blog about video games",
+};
 
-export default function Blog() {
+async function getData() {
+  const res = await fetch(`${process.env.API_URL}/post-games/?populate=*`);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+ 
+  return res.json();
+}
+
+export default async function Blog() {
+  const  {data} = await getData();
+
   return ( 
     <>
       <h1 className="text-6xl font-bold text-center text-sky-700">Blog</h1> 
-  
     </>
   )
 }
