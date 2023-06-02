f99328c1796b8f49889d22fa9b4438f6c1aeeb73
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-06-02 18:55:51 +0200

    [TASK] add domain in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        domains: ['res.cloudinary.com']
+    }
+}
 
 module.exports = nextConfig
