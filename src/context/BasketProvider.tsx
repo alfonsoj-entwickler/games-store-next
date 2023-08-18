f6b30186aac15fd9c933ff183478f606b8fb75a2
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-08-18 19:31:56 +0200

    [TASK] delete console log

--- src/context/BasketProvider.tsx
+++ src/context/BasketProvider.tsx
@@ -6,10 +6,16 @@ import Games from "@/types/Games";
 export const BasketContext = createContext({});
 
 export default function BasketProvider({ children }: any) {
-  const [basketGamesData, setBasketGamesData] = useState<Array<Games> | undefined>([]);
+  const [basketGamesData, setBasketGamesData] = useState<Array<Games>>([]);
 
-  const handleBasketData = () => {
-    console.log("Provider Basket ...");
+  const handleBasketData = (item:Games) => {
+    
+    setBasketGamesData(
+      [
+        ...basketGamesData.filter(game => game.id !== item.id),
+        item
+      ]
+    );
   };
 
 
